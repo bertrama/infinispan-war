c58a704c0fed569da1f719d3e45d863788e5624e
bertrama <bertrama@umich.edu>
2015-02-20 11:54:48 -0500

    Making this more robust.

--- bin/start.sh
+++ bin/start.sh
@@ -7,30 +7,37 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   SOURCE="$(readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-CONFIG="$(dirname $DIR)/configs/config.xml"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && dirname "$(pwd)" )"
+CONFIG="$DIR/configs/config.xml"
+WAR="$DIR/target/infinispan.war"
 
-MACHINE=$(hostname)
 if [ x"$MACHINE" = x"" ] ; then
-  MACHINE=MACHINE
+  MACHINE=$(/bin/hostname)
+  if [ x"$MACHINE" = x"" ] ; then
+    MACHINE=MACHINE
+  fi
 fi
 
-RACK=$(ifconfig | grep "$(hostname -I)" | sed -e 's/^.*Bcast://' -e 's/ .*//')
 if [ x"$RACK" = x"" ] ; then
-  RACK=RACK
+  RACK=$(/sbin/ifconfig | grep "$(/bin/hostname -I | awk '{print $1}')" | sed -e 's/^.*Bcast://' -e 's/ .*//')
+  if [ x"$RACK" = x"" ] ; then
+    RACK=RACK
+  fi
 fi
 
-SITE=$(host $(route -n | grep '^0.0.0.0' | awk '{print $2}') | grep 'domain name pointer' | awk '{print $5}' | sed -e 's/^[^.]*\.//' -e 's/\.$//')
 if [ x"$SITE" = x"" ] ; then
-  SITE=$(route -n | grep '^0.0.0.0' | awk '{print $2}')
+  SITE=$(/usr/bin/host $(/sbin/route -n | grep '^0.0.0.0' | awk '{print $2}') | grep 'domain name pointer' | awk '{print $5}' | sed -e 's/^[^.]*\.//' -e 's/\.$//')
+  if [ x"$SITE" = x"" ] ; then
+    SITE=$(/sbin/route -n | grep '^0.0.0.0' | awk '{print $2}')
+  fi
 fi
 
 case "$1" in
   left)
     ARGS="
   -Dinfinispan.config=$CONFIG
-  -Dinfinispan.ajp.port=8009
-  -Dinfinispan.http.port=8080
+  -Dinfinispan.ajp.port=8010
+  -Dinfinispan.http.port=8081
   -Dinfinispan.hotrod.port=11222
   -Dinfinispan.memcached.port=11211
   -Dinfinispan.transport.tcp.node=left
@@ -42,8 +49,8 @@ case "$1" in
   right)
     ARGS="
   -Dinfinispan.config=configs/config.xml
-  -Dinfinispan.ajp.port=8010
-  -Dinfinispan.http.port=8081
+  -Dinfinispan.ajp.port=8011
+  -Dinfinispan.http.port=8082
   -Dinfinispan.hotrod.port=11223
   -Dinfinispan.memcached.port=11212
   -Dinfinispan.transport.tcp.node=right
@@ -58,4 +65,4 @@ case "$1" in
   ;;
 esac
 
-java -jar $ARGS target/infinispan.war
+java $ARGS -jar $WAR
